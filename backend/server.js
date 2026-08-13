3d3d73146322f36823c248d87b3bfea0c0e6e851
Hitak Suthar <hitaksuthar9680@gmail.com>
2026-08-13 18:04:37 +0530

    Fix server startup

--- backend/server.js
+++ backend/server.js
@@ -27,17 +27,40 @@ app.get("/", (req, res) => {
   res.send("Portfolio backend is running.");
 });
 
+
+
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("Connected to MongoDB");
-    app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB connection error:", error);
   });
+
+
+
+
+
+
+
+// const PORT = process.env.PORT || 5000;
+
+
+// mongoose
+//   .connect(process.env.MONGO_URI)
+//   .then(() => {
+//     console.log("Connected to MongoDB");
+//     app.listen(PORT, () => {
+//       console.log(`Server is running on http://localhost:${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error("MongoDB connection error:", error);
+//   });
